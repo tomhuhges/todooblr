9aa539b33bb291d90add830505c7e8e8c3faaab7
tomhuhges <merzbear@gmail.com>
2017-01-06 14:29:38 +0000

    reorganise

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,6 @@
 var gulp = require('gulp'),
 	connect = require('gulp-connect');
-	babel = require('gulp-babel');
 
-var paths = [
-	{ "/assets": "assets/*" },
-	{ "/css": "css/*" },
-	{ "/": "index.html" }
-];
-
-gulp.task('copy', function(){
-	paths.forEach(function(src){
-		var key = Object.keys(src)[0];
-		gulp.src("src/" + src[key])
-	    .pipe(gulp.dest("dist" + key));
-	});
-});
- 
-gulp.task('webserver', function() {
+gulp.task('default', function() {
 	connect.server();
 });
-
-gulp.task('babel', function () {
-  return gulp.src("src/js/**/*.js")
-    .pipe(babel())
-    .pipe(gulp.dest("dist/js"));
-});
-
-gulp.task('default', ['webserver', 'babel', 'copy']);
